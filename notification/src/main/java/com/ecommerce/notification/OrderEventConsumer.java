35eab1d4db1e41b98c1a64905beda26f4d058eb0
Amos Uzuagie <amosuzuagie@gmail.com>
2025-07-01 08:54:43 +0100

    feat: migrate RabbitMQ and Notification service to Spring Cloud Stream
    - Replaced direct RabbitMQ integration with Spring Cloud Stream bindings
    - Updated Notification service to use StreamListener/functional style messaging
    - Refactored message configuration to use application.yml stream bindings
    - Improved decoupling and simplified message publishing/subscribing

--- notification/src/main/java/com/ecommerce/notification/OrderEventConsumer.java
+++ notification/src/main/java/com/ecommerce/notification/OrderEventConsumer.java
@@ -1,30 +1,38 @@
 package com.ecommerce.notification;
 
 import com.ecommerce.notification.payload.OrderCreatedEvent;
-import com.ecommerce.notification.payload.OrderStatus;
-import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
-import java.util.Map;
+import java.util.function.Consumer;
 
+@Slf4j
 @Service
 public class OrderEventConsumer {
 
-    @RabbitListener(queues = "${rabbitmq.queue.name}")
-    public void handleOrderEvent(OrderCreatedEvent orderEvent) {
-        System.out.println("Receive Order Event: " + orderEvent);
-
-        Long orderId = orderEvent.getOrderId();
-        OrderStatus status = orderEvent.getStatus();
-
-        System.out.println("Order ID: " + orderId);
-        System.out.println("Order Status: " + status);
-
-        // todo: Send Notification
-        // todo: Send Emails
-        // todo: Generate Invoice
-        // todo: Update Database
-        // todo: Send Seller Notification
+    @Bean
+    public Consumer<OrderCreatedEvent> createdOrder() {
+        return event -> {
+            log.info("Received order event for order ID: {}, & user ID: {}", event.getOrderId(), event.getUserId());
+        };
     }
+
+//    @RabbitListener(queues = "${rabbitmq.queue.name}")
+//    public void handleOrderEvent(OrderCreatedEvent orderEvent) {
+//        System.out.println("Receive Order Event: " + orderEvent);
+//
+//        Long orderId = orderEvent.getOrderId();
+//        OrderStatus status = orderEvent.getStatus();
+//
+//        System.out.println("Order ID: " + orderId);
+//        System.out.println("Order Status: " + status);
+//
+//        // todo: Send Notification
+//        // todo: Send Emails
+//        // todo: Generate Invoice
+//        // todo: Update Database
+//        // todo: Send Seller Notification
+//    }
 
 }
