520bee31a7d0be3db2e69f3bb90c9507f05aeb76
Amos Uzuagie <amosuzuagie@gmail.com>
2025-07-26 11:42:18 +0100

    chore: containerize app using spring-boot-maven-plugin buildpacks

---
+++ deploy/docker/build-images-buildpack.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd ../..
+cd user && ./mvnw spring-boot:build-image -DskipTests && cd ..
+cd order && ./mvnw spring-boot:build-image -DskipTests && cd ..
+cd eureka && ./mvnw spring-boot:build-image -DskipTests && cd ..
+cd product && ./mvnw spring-boot:build-image -DskipTests && cd ..
+cd gateway && ./mvnw spring-boot:build-image -DskipTests && cd ..
+cd configserver && ./mvnw spring-boot:build-image -DskipTests && cd ..
+cd notification && ./mvnw spring-boot:build-image -DskipTests && cd ..
+
